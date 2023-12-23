5adc55afbaf7b0fb5bb07ee74b6291be9cd8d2c2
reejith-remanan <reejithremanan@gmail.com>
2023-12-23 21:01:19 +0530

    till textbox and add button

---
+++ README.md
@@ -0,0 +1,4 @@
+<<<<<<< HEAD
+=======
+# tasky-app-using-react
+>>>>>>> 1497ba7ac6a51fd5f0408445a6aab7e71a47949b
---
+++ src/App.js
@@ -0,0 +1,65 @@
+// import logo from './logo.svg';
+import './App.css';
+
+import React, { Component } from 'react'
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      currentItem: {
+        text: "",
+        key: ""
+      }
+    };
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
+
+  }
+
+  handleInput(e) {
+    this.setState({
+      currentItem: {
+        text: e.target.value,
+        key: Date.now(),
+      },
+    });
+  }
+  addItem(e) {
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    console.log(newItem);
+    if (newItem.text !== "") {
+
+      const newItems = [...this.state.items, newItem];
+      this.setState({
+        items: newItems,
+        currentItem: {
+          text: '',
+          key: ''
+        }
+      })
+    }
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <form id="to-do-form" onSubmit={this.addItem}>
+            <input
+              type="text"
+              placeholder="Enter Text"
+              value={this.state.currentItem.text}
+              onChange={this.handleInput}
+            />
+            <button type="submit">Add</button>
+          </form>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
